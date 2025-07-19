0fad18794b7e23f10ef7df1fa016f4b997589d9d
sungyeon6 <0504sypark@naver.com>
2025-07-19 23:51:59 +0900

    [FEAT] 회원가입(api 연결 전)

---
+++ lib/nickname.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Nickname extends StatelessWidget {
+  const Nickname({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NicknamePage extends StatefulWidget {
+  const NicknamePage({super.key});
+
+  @override
+  State<NicknamePage> createState() => _NicknamePageState();
+}
+
+class _NicknamePageState extends State<NicknamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/register.dart
+++ lib/register.dart
@@ -22,6 +22,105 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Doki',
+              style: TextStyle(
+                fontSize: 110,
+                fontFamily: 'Jersey15-Regular',
+                color: Color(0xff69BF70),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(24),
+              width: 280,
+              height: 280,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  color: const Color(0xffD9D9D9),
+                  width: 2,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 8,
+                    spreadRadius: 0,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  const TextField(
+                    cursorColor: Color(0x80000000),
+                    decoration: InputDecoration(
+                      labelText: '이메일',
+                      labelStyle: TextStyle(
+                        fontFamily: 'BlackHanSans-Regular',
+                        color: Color(0x80000000),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0x80000000))
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Color(0x80000000))
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const TextField(
+                    obscureText: true,
+                    cursorColor: Color(0x80000000),
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      labelStyle: TextStyle(
+                        fontFamily: 'BlackHanSans-Regular',
+                        color: Color(0x80000000),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0x80000000))
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0x80000000))
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        backgroundColor: const Color(0xff69BF70),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder: (context, animation, secondaryAnimation) => const Nickname(),
+                          ),
+                        );
+                      },
+                      child: Padding(padding: padding),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
