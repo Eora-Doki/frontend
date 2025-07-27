4074084984070462b62f51a2b97e73b9f21ffbfc
sungyeon6 <0504sypark@naver.com>
2025-07-28 03:22:27 +0900

    [FEAT] 인벤토리 모달창

--- lib/screens/avatar/inventory.dart
+++ lib/screens/avatar/inventory.dart
@@ -1,20 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class InventoryModal extends StatelessWidget {
+import '../../controllers/user_controller.dart';
+import 'inventory_items.dart';
+
+class InventoryModal extends StatefulWidget {
   const InventoryModal({super.key});
+
+  @override
+  State<InventoryModal> createState() => _InventoryModalState();
+}
+
+class _InventoryModalState extends State<InventoryModal> {
+  int selectedIndex = -1;
+  String selectedCategory = '아바타';
+  List<String> itemList = [];
+
+  final userController = Get.find<UserController>();
+
+  final List<String> avatarList = [
+    'assets/images/avatars/sf_anya.png',
+    'assets/images/avatars/sf_loid.png',
+    'assets/images/avatars/sf_bond.png',
+  ];
+
+  final List<String> conceptList = [
+    'assets/images/concepts/sf_anya_1.png',
+    'assets/images/concepts/sf_anya_2.png',
+    'assets/images/concepts/sf_anya_3.png',
+    'assets/images/concepts/sf_anya_4.png',
+  ];
+
+  final List<String> backgroundList = [
+    'assets/images/backgrounds/bg_at.png',
+    'assets/images/backgrounds/bg_fr.png',
+    'assets/images/backgrounds/bg_sf.png',
+  ];
+
+  // 처음 열었을 때 아바타 리스트 보여줌
+  @override
+  void initState() {
+    super.initState();
+    itemList = avatarList;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 300,
+      width: 500,
       decoration: const BoxDecoration(
         color: Color(0xffB2ECB7),
-        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
       ),
       child: Center(
-        child: Text(
-          '인벤토리',
-          style: TextStyle(fontSize: 24, fontFamily: 'BlackHanSans-Regular'),
+        child: Column(
+          children: [
+            const SizedBox(height: 15),
+            const Text(
+              '기본 아바타를 보유하고 있어야 컨셉을 구매할 수 있습니다!',
+              style: TextStyle(
+                fontFamily: 'BlackHanSans-Regular',
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff69BF70),
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        selectedCategory = '아바타';
+                        itemList = avatarList;
+                        selectedIndex = -1;
+                      });
+                    },
+                    child: const Text(
+                      '아바타',
+                      style: TextStyle(
+                        fontFamily: 'BlackHanSans-Regular',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(width: 30),
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff69BF70),
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        selectedCategory = '컨셉';
+                        itemList = conceptList;
+                        selectedIndex = -1;
+                      });
+                    },
+                    child: const Text(
+                      '컨셉',
+                      style: TextStyle(
+                        fontFamily: 'BlackHanSans-Regular',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(width: 30),
+                SizedBox(
+                  width: 100,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff69BF70),
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        selectedCategory = '배경';
+                        itemList = backgroundList;
+                        selectedIndex = -1;
+                      });
+                    },
+                    child: const Text(
+                      '배경',
+                      style: TextStyle(
+                        fontFamily: 'BlackHanSans-Regular',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 10),
+            Expanded(
+              child: InventoryItems(
+                  imagePath: itemList,
+                  selectedIndex: selectedIndex,
+                  onSelect: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  }
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/screens/avatar/inventory_items.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class InventoryItems extends StatelessWidget {
+  final List<String> imagePath;
+  final int selectedIndex;
+  final void Function(int) onSelect;
+
+  const InventoryItems({
+    super.key,
+    required this.imagePath,
+    required this.selectedIndex,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: GridView.builder(
+        itemCount: imagePath.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 10,
+          childAspectRatio: 1,
+        ),
+        itemBuilder: (context, index) {
+          final isSelected = index == selectedIndex;
+
+          return GestureDetector(
+            onTap: () => onSelect(index),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      imagePath[index],
+                      width: 80,
+                      height: 80,
+                    ),
+                  ),
+
+                  if (isSelected)
+                    Positioned(
+                      bottom: -13,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff69BF70),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            '선택',
+                            style: TextStyle(
+                              fontFamily: 'BlackHanSans-Regular',
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
