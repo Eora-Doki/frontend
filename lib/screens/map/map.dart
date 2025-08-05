4b27d2c041df2a232bd4144a9f429253ab712b3c
sungyeon6 <0504sypark@naver.com>
2025-08-05 16:15:49 +0900

    [FEAT] 현재 위치 반환 기능

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../services/location_service.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -14,9 +17,24 @@ class _MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
+    _loadHtml();
+  }
 
+  Future<void> _loadHtml() async {
+    final htmlMap = await rootBundle.loadString('assets/kakao_map.html');
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'ready',
+        onMessageReceived: (message) async {
+          final position = await LocationService.getCurrentLocation();
+          if (position != null) {
+            final latitude = position['latitude'];
+            final longitude = position['longitude'];
+            await _controller.runJavaScript("updateLocation($latitude, $longitude);");
+          }
+        },
+      )
       ..setNavigationDelegate(NavigationDelegate(
         onWebResourceError: (error) {
           print("WebView error: ${error.description}");
@@ -25,43 +43,11 @@ class _MapPageState extends State<MapPage> {
           print("Page finished loading: $url");
         },
       ))
-      ..loadHtmlString('''
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Kakao Map</title>
-          <style>
-            html, body { margin: 0; padding: 0; height: 100%; }
-            #map { width: 100%; height: 100%; }
-          </style>
-        </head>
-        <body>
-          <div id="map"></div>
+      ..loadHtmlString(htmlMap);
 
-          <script type="text/javascript">
-            document.addEventListener("DOMContentLoaded", function() {
-              var script = document.createElement('script');
-              script.onload = function() {
-                kakao.maps.load(function() {
-                  var mapContainer = document.getElementById('map');
-                  var mapOption = {
-                    center: new kakao.maps.LatLng(37.5665, 126.9780),
-                    level: 3
-                  };
-                  var map = new kakao.maps.Map(mapContainer, mapOption);
-                });
-              };
-              script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=d172d22c60e3873dff49ddbd4c0526fb&autoload=false";
-              document.head.appendChild(script);
-            });
-          </script>
-        </body>
-        </html>
-      ''');
-
-
+    setState(() {}); // 웹뷰 리빌드
   }
+
 
   @override
   Widget build(BuildContext context) {
